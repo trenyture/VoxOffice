dae6814b2ef40a34cc0060dc72390a49e5b96a50
trenyture <simon.trichereau@gmail.com>
2016-01-20 13:48:37 -0600

    Installation du projet

--- index.php
+++ index.php
@@ -1,12 +1,64 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<!--BALISES META-->
 	<meta charset="UTF-8" />
 	<meta name="author" content="Pierre Prézelin - Simon Trichereau" />
 	<meta name="description" content="Site web pour choisir et voter pour le meilleur film!" />
+	<meta name="keyword" content="Meilleur,Film,Best,Movie,Simon,Trichereau,Pierre,Prezelin,VoxOffice,Voter,Vote,HTML,CSS,JavaScript,jQuery,Facebook,Share,Partage,Php,MySQL,SASS,Compass" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<!--TITLE-->
 	<title>Vox Office</title>
+	<!--LINK TO CSS-->
+	<link rel="stylesheet" type="text/css" href="css/reset.css" />
+	<link rel="stylesheet" type="text/css" href="css/style.css" />
+	<!--LINK TO JS-->
+	<script type="text/javascript"></script>
+
 </head>
 <body>
-
+	<header>
+		<nav>
+			<ul>
+				<li>Home</li>
+				<li>Vote</li>
+				<li>Ratings</li>
+				<li>Add</li>
+			</ul>
+		</nav>
+		<div>
+			<img src="#" alt="User - VoxOffice" title="User - VoxOffice" />
+			<p>Connected as:<br/><span id="name">Pierre Prézelin</span></p>
+		</div>
+	</header>
+	<section class="sized">
+		<h1><a href="#" title="VoxOffice - Home" alt="VoxOffice - Home">VoxOffice</a></h1>
+		<article>
+			<p>All your favorite movies. Classified!</p>
+		</article>
+		<a href="#">Go!</a>
+		<img src="#" alt="Scroll - VoxOffice" title="Scroll - VoxOffice" />
+	</section>
+	<section class="sized">
+		
+	</section>
+	<section class="sized">
+		
+	</section>
+	<section>
+		<h2>Get Started</h2>
+		<p>By connecting with Facebook we can directly help you and your friends find the greatest films!</p>
+		<p class="alert">This, without ads, tracking or annoying notifications.</p>
+	</section>
+	<footer>
+		<div class="wrap">
+			<ul>
+				<li>Versions</li>
+				<li>FAQ</li>
+				<li>Contact</li>
+			</ul>
+			<p>by Simon-tr and Pierre Prezelin. Copyright 2016</p>
+		</div>
+	</footer>
 </body>
 </html>
