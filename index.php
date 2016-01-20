abe6507f06a119d956d24f7971b1ddfbac95070c
trenyture <simon.trichereau@gmail.com>
2016-01-20 13:32:17 -0600

    Installation Projet

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8" />
+	<meta name="author" content="Pierre Prézelin - Simon Trichereau" />
+	<meta name="description" content="Site web pour choisir et voter pour le meilleur film!" />
+	<title>Vox Office</title>
+</head>
+<body>
+
+</body>
+</html>
