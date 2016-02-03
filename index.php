f8c6972004110a16b9b0b89cfa0c9a6fc537b766
OldsunFlush <pierre.prez2@gmail.com>
2016-02-03 23:21:07 +0100

    Homepage global styles (header, footer, sections, titles, texts)

--- index.php
+++ index.php
@@ -50,75 +50,82 @@
     </head>
     <body class="home">
         <header>
+            <a href="#" class="logo"><img src="img/logo.svg" alt="VoxOffice"></a>
             <nav>
                 <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">Vote</a></li>
-                    <li><a href="#">Ratings</a></li>
-                    <li><a href="#">Add</a></li>
+                    <li><a href="#">Accueil</a></li>
+                    <li><a href="#">Voter</a></li>
+                    <li><a href="#">Classement</a></li>
+                    <li><a href="#">Ajouter</a></li>
                 </ul>
             </nav>
-            <div>
+            <div class="user">
                 <img src="#" alt="Utilisateur" title="Utilisateur" />
-                <p>Connecté en tant que :<br/><span id="username" class="uppercase highlight">Pierre Prézelin</span></p>
+                <p>Connecté en tant que :<br/><span id="username">Pierre Prézelin</span></p>
             </div>
         </header>
         <section class="home-intro">
-            <h1><a href="#" title="VoxOffice - Accueil" alt="VoxOffice - Accueil">Vox<span>Office</span></a></h1>
-            <article>
-                <p>Tous vos films favoris. Classés.</p>
-            </article>
-            <a href="#" class="btn btn-home">Go!</a>
+            <div class="intro-content">
+                <h1><a href="#" title="VoxOffice - Accueil" alt="VoxOffice - Accueil">Vox<span>Office</span></a></h1>
+                <p class="subtitle">Tous vos films favoris. Classés.</p>
+                <a href="#" class="btn btn-home">Go!</a>
+            </div>
             <a href="#home-description"><span></span></a>
         </section>
         <section class="home-description">
-            <div class="content-large">
-                <h2>Le concept.</h2>
-                <p>VoxOffice est un projet visant la création d'une classification ultra rapide de milliers de films. Pour atteindre cet objectif, <span class="uppercase">Vous</span> êtes le principal acteur, en pouvant <span class="uppercase">voter</span>, <span class="uppercase">ajouter</span> et <span class="uppercase">comparer</span> tous les films que vous voulez.</p>
-                <p>Choisissez rapidement parmi deux films proposés, pour créer un top des "meilleurs" et des "pires".</p>
-                <p>Nous nous chargeons du reste. <span class="majuscule">Aussi simple que cela!</span></p>
-                <p>Vous pouvez lire la <a href="#">documentation</a> pour plus d'informations ou sur le <a href="#">dépôt officiel GitHub</a>.</p>
+            <div class="content-lg">
+                <div class="description half">
+                    <h2>Le concept.</h2>
+                    <p>VoxOffice est un projet visant la création d'une classification ultra rapide de milliers de films. Pour atteindre cet objectif, <span class="uppercase">Vous</span> êtes le principal acteur, en pouvant <span>voter</span>, <span>ajouter</span> et <span>comparer</span> tous les films que vous voulez.</p>
+                    <p>Choisissez rapidement parmi deux films proposés, pour créer un top des "meilleurs" et des "pires".</p>
+                    <p class="bigger">Nous nous chargeons du reste. <br><span>Aussi simple que cela!</span></p>
+                    <p>Vous pouvez lire la <a href="https://github.com/trenyture/VoxOffice" target="_blank">documentation</a> pour plus d'informations ou sur le <a href="https://github.com/trenyture/VoxOffice" target="_blank">dépôt officiel GitHub</a>.</p>
+                </div>
             </div>
         </section>
         <section class="home-features">
-            <div class="content">
+            <div class="content-md">
                 <h2>Comment faire</h2>
                 <ul>
-                    <li>
+                    <li class="feature feature-vote">
                         <h3>Votez.</h3>
-                        <img src="#" alt="Votez - VoxOffice" title="Votez - VoxOffice" />
-                        <p>Lorem ipsum dolor sit ament, consectetur adipiscing elit.</p>
+                        <p>Vestibulum vehicula sollicitudin dolor sit amet ornare.</p>
+                        <p>Nullam nec dictum eros. Sed nisi ante, vestibulum sit amet viverra eget, sollicitudin ac dui. Aliquam nibh velit.</p>
                         <a href="#" alt="Essayer - VoxOffice" title="Essayer - VoxOffice">Essayer</a>
+                        <img src="img/vote.svg" alt="Votez - VoxOffice" title="Votez - VoxOffice" />
                     </li>
-                    <li>
+                    <li class="feature feature-add">
                         <h3>Ajoutez.</h3>
-                        <img src="#" alt="Ajoutez - VoxOffice" title="Ajoutez - VoxOffice" />
-                        <p>Lorem ipsum dolor sit ament, consectetur adipiscing elit.</p>
+                        <p>Vestibulum vehicula sollicitudin dolor sit amet ornare. Nullam nec dictum eros. Sed nisi ante, vestibulum sit amet viverra eget, sollicitudin ac dui. Aliquam nibh velit.</p>
                         <a href="#" alt="Essayer - VoxOffice" title="Essayer - VoxOffice">Essayer</a>
+                        <img src="img/add.svg" alt="Ajoutez - VoxOffice" title="Ajoutez - VoxOffice" />
                     </li>
-                    <li>
+                    <li class="feature feature-compare">
                         <h3>Comparez.</h3>
-                        <img src="#" alt="Comparz - VoxOffice" title="Comparez - VoxOffice" />
-                        <p>Lorem ipsum dolor sit ament, consectetur adipiscing elit.</p>
+                        <p>Vestibulum vehicula sollicitudin dolor sit amet ornare. Nullam nec dictum eros. Sed nisi ante, vestibulum sit amet viverra eget, sollicitudin ac dui.</p>
+                        <p>Aliquam nibh velit.</p>
                         <a href="#" alt="Essayer - VoxOffice" title="Essayer - VoxOffice">Essayer</a>
+                        <img src="img/compare.svg" alt="Comparz - VoxOffice" title="Comparez - VoxOffice" />
                     </li>
                 </ul>
             </div>
         </section>
         <section class="home-connect">
-            <div class="content">
+            <div class="content-md">
                 <h2>Lancez-vous</h2>
-                <p>En vous connectant à Facebook, nous pouvez directement vous aider vous et vos amis à trouver les plus grands films !</p>
+                <div class="content-sm">
+                    <p><small>En vous connectant à Facebook, nous pouvez directement vous aider vous et vos amis à trouver les plus grands films !</small></p>
+                </div>
                 <p class="alert">Ce, sans aucune publicité, tracking, ou notifications intrusives.</p>
-                <a href="#" alt="Connexion à Facebook - VoxOffice" title="Connexion à Facebook - VoxOffice"></a>
+                <a href="#" alt="Connexion à Facebook - VoxOffice" title="Connexion à Facebook - VoxOffice" class="btn btn-fb"></a>
             </div>
         </section>
         <footer>
-            <div class="content-large">
+            <div class="content-lg">
                 <ul>
-                    <li>Versions</li>
-                    <li>FAQ</li>
-                    <li>Contact</li>
+                    <li><a href="#">Versions</a></li>
+                    <li><a href="#">FAQ</a></li>
+                    <li><a href="#">Contact</a></li>
                 </ul>
                 <p><span class="icon icon-lab"></span>par Simon-tr et Pierre Prezelin. Copyright 2016</p>
             </div>
