c4ea6823968219f6cd1e748ca4c717e81421d0fd
Pierre Prézelin <pierre.prez2@gmail.com>
2016-07-16 20:09:28 +0200

    Setting the 'Vote' page markup

--- vote.php
+++ vote.php
@@ -1,28 +1,26 @@
-<?php 
-	require_once('assets/php/header.php'); 
-?>
+<?php require_once('assets/php/header.php'); ?>
 	<section>
-        <h2>Quel est ton favori?</h2>
-        <article id="film1">
-            <h3></h3>
-            <p></p>
-            <a href="#" class="vote plus">Je vote +</a>
-            <a href="#" class="vote moins">Je vote -</a>
-            <span>Share
-                <a href="#" class="vote plus" id="sh-f1-plus">Je vote +</a>
-                <a href="#" class="vote moins" id="sh-f1-moins">Je vote -</a>
-            </span>
+        <h1>Votez pour votre favori</h1>
+        <h2>Aucune réponse n'est mauvaise !</h2>
+        <article id="film1" class="film-vote">
+            <h3>"[Nom de mon film 1]"</h3>
+            <p class="author"><em>[Prénom] [Nom]</em></p>
+            <p class="date">[Date]</p>
+            <a href="#" class="vote plus"><i class="fa fa-check"></i> Voter</a>
+            <!--<a href="#" class="vote moins">Je vote -</a>-->
+            <a href="#" class="vote share" id="sh-f1-plus"><i class="fa fa-facebook"></i>Voter et partager</a>
+            <!--<a href="#" class="vote moins" id="sh-f1-moins">Je vote -</a>-->
+            <a href="#" id="others">Passer <i class="fa fa-arrow-right"></i></a>
         </article>
-        <a href="#" id="others">Autres films</a>
-        <article id="film2">
-            <h3></h3>
-            <p></p>
-            <a href="#" class="vote plus">Je vote +</a>
-            <a href="#" class="vote moins">Je vote -</a>
-            <span>Share
-                <a href="#" class="vote plus" id="sh-f2-plus">Je vote +</a>
-                <a href="#" class="vote moins" id="sh-f2-moins">Je vote -</a>
-            </span>
+        <article id="film2" class="film-vote">
+            <h3>"[Nom de mon film 2]"</h3>
+            <p class="author"><em>[Prénom] [Nom]</em></p>
+            <p class="date">[Date]</p>
+            <a href="#" class="vote plus"><i class="fa fa-check"></i> Voter</a>
+            <!--<a href="#" class="vote moins">Je vote -</a>-->
+            <a href="#" class="vote share" id="sh-f2-plus">Voter et partager</a>
+            <!--<a href="#" class="vote moins" id="sh-f2-moins">Je vote -</a>-->
+            <a href="#" id="others">Passer <i class="fa fa-arrow-right"></i></a>
         </article>
         <!-- petite precision sur les id des liens : f1 = film 1 et sh pour le bouton share : celui qu'on implémentera avec facebook! -->
         <!-- seconde precision : le contenu se fera en ajax donc dynamiquement (process : chercher base de donnée 2 films randoms et les afficher) -->
