be196b5ff89f69483456ebd763a7ff4d1eed3b25
Simon Tricherau <strichereau@centralmedia.mx>
2016-08-08 13:31:55 -0500

    Changement sé

--- assets/php/addvote.php
+++ assets/php/addvote.php
@@ -1,18 +1,24 @@
 <?php
 	ini_set('display_errors', 1);  error_reporting(E_ALL);
     session_start();
+	include('newPDO.php');
 	if(!isset($_SESSION['fb_token'])){
 		header('location:../../index');
-	}
-	var_dump($_GET);
-	include('newPDO.php');
-	$maBD = connexionBD();
-	$requExec = 'UPDATE VO_films SET vote = vote + 1 WHERE id ='.$_GET['id'];
-	$requete = $maBD->prepare($requExec);
-	$ok = $requete->execute();
-	if($ok == 1){
-		header('location:../../vote');
 	}else{
-		echo 'Bien joué vous avez tout fait bugguer!!!';
+	    $error;
+	    $msg = '';
+		$maBD = connexionBD();
+		$requExec = 'UPDATE VO_films SET vote = vote + 1 WHERE id ='.$_GET['id'];
+		$requete = $maBD->prepare($requExec);
+		$ok = $requete->execute();
+		if($ok == 1){
+			$error = false;
+			$msg = '';
+		}else{
+			$error = true;
+			$msg = 'Vous avez fait planté la base de donnée';
+		}
+		$result = array($error, $msg);
+		echo json_encode($result);
 	}
 ?>
