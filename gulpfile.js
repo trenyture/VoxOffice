d8e352fc14abddb3a174c9215e71b9d812cf7b36
Jiedara <m.villanove@laposte.net>
2016-11-05 21:23:37 +0100

    update gulpfile to handle compass

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
 var elixir = require('laravel-elixir');
 
+require('laravel-elixir-sass-compass');
+
 elixir(function(mix) {
-    mix.sass('../../scss/style.scss')
-       .webpack('../../js/script.js');
+    mix.compass('../../scss/style.scss', 'public/css/', {
+		style:"compressed",
+		sass:"./resources/scss",
+	})
+    .scriptsIn('../../js','publicscript.js');
 });
